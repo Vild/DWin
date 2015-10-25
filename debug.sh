0468c4754a272cdbd6bb4ab8bf185757401f12af
Dan Printzell <xwildn00bx@gmail.com>
2015-10-25 02:47:43 +0200

    Made debug script better
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- debug.sh
+++ debug.sh
@@ -3,7 +3,7 @@ TITLE="DWin - D always win!"
 SIZE="1280x720"
 XPORT=":8"
 
-dub build
+dub build || exit 1
 
 Xephyr -name "$TITLE" -ac -br -noreset -screen "$SIZE" "$XPORT" >/dev/null 2>&1 &
 XEPHYRPID=$!
@@ -11,6 +11,7 @@ XEPHYRPID=$!
 sleep 1
 DISPLAY=$XPORT
 
+feh --bg-max ~/Pictures/Wallpaper.arch/archwall_dark_purple.png
 xterm &
 xterm &
 #lxterminal &
