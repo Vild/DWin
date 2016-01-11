52ba52c4ee33de18de84d15206df865c4a632b5b
Dan Printzell <xwildn00bx@gmail.com>
2016-01-11 20:54:21 +0100

    Added spawing of programs into app.d, removed debug.sh
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- source/app.d
+++ source/app.d
@@ -1,5 +1,6 @@
 import std.getopt;
 import dwin.dwin;
+import std.process;
 
 int main(string[] args) {
 	import std.stdio : writeln, writefln;
@@ -11,19 +12,44 @@ int main(string[] args) {
 		return 0;
 	}
 
+	string title = "DWin - D always win!";
+	string size = "1280x720";
+
+	auto Xephyr = spawnProcess([`Xephyr`, `-name`, title, `-ac`, `-br`, `-noreset`, `-screen`, size, `:8`]);
+	scope (exit)
+		kill(Xephyr);
+
+	import core.thread : Thread;
+	import core.time : seconds;
+
+	Thread.sleep(1.seconds);
+
 	auto dwin = new DWin();
 	scope (exit)
 		dwin.destroy;
 
-	import std.process : spawnProcess, kill;
+	auto childEnv = environment.toAA;
+	childEnv["DISPLAY"] = ":8";
 
-	auto xeyes = spawnProcess("xeyes");
+	Pid xeyes;
+	Pid xterm;
+
 	scope (exit)
 		kill(xeyes);
-
-	auto xterm1 = spawnProcess("xterm");
 	scope (exit)
-		kill(xterm1);
+		kill(xterm);
+
+	auto spawnThread = new Thread(() {
+		Thread.sleep(1.seconds);
+		spawnProcess(["feh", "--bg-scale", "http://wild.tk/DWinBG.png"], childEnv);
+		xeyes = spawnProcess("xeyes", childEnv);
+		xterm = spawnProcess("xterm", childEnv);
+
+		wait(xeyes);
+		wait(xterm);
+	});
+
+	spawnThread.start();
 
 	dwin.Run();
 
