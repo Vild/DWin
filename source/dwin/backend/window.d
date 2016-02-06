c21bf37dcd31bc6dacd3ec59ec09d01ec8f53df7
Dan Printzell <xwildn00bx@gmail.com>
2016-02-06 23:41:16 +0100

    [Window] Added Strut and Workspace vars
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- source/dwin/backend/window.d
+++ source/dwin/backend/window.d
@@ -2,7 +2,24 @@ module dwin.backend.window;
 
 import dwin.backend.layout;
 import dwin.backend.container;
+import dwin.backend.workspace;
 import dwin.log;
+
+struct Strut {
+	uint left;
+	uint right;
+	uint top;
+	uint bottom;
+
+	uint left_start_y;
+	uint left_end_y;
+	uint right_start_y;
+	uint right_end_y;
+	uint top_start_x;
+	uint top_end_x;
+	uint bottom_start_x;
+	uint bottom_end_x;
+}
 
 abstract class Window : Container {
 public:
@@ -27,4 +44,30 @@ public:
 
 	@property abstract string Title();
 	//@property abstract bool IsVisible();
+
+	override string toString() {
+		import std.format : format;
+
+		return format("%s %s: %s", cast(void*)this, typeid(this).name, Title);
+	}
+
+	@property.Strut Strut() {
+		return strut;
+	}
+
+	@property uint Desktop() {
+		return desktop;
+	}
+
+	@property ref.Workspace Workspace() {
+		return workspace;
+	}
+
+	@property abstract bool IsDock();
+	@property abstract bool IsSticky();
+
+protected:
+	.Strut strut;
+	uint desktop;
+	.Workspace workspace;
 }
