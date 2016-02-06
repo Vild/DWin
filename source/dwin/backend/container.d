2bd8c08a252767b0e728d735ae9795f7c8782e8f
Dan Printzell <xwildn00bx@gmail.com>
2016-02-06 23:39:36 +0100

    [Container] Calculate a grid position
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- source/dwin/backend/container.d
+++ source/dwin/backend/container.d
@@ -4,12 +4,22 @@ import dwin.backend.layout;
 import dwin.backend.window;
 import dwin.backend.screen;
 
+enum PositionPart {
+	First,
+	Second,
+	Third
+}
+
+struct GridPosition {
+	PositionPart x;
+	PositionPart y;
+}
+
 abstract class Container {
 public:
 	abstract void Add(Container container);
 	abstract void Remove(Container container);
 	abstract void Move(short x, short y) {
-
 	}
 
 	abstract void Resize(ushort width, ushort height);
@@ -22,6 +32,39 @@ public:
 
 	abstract void Update();
 	abstract void Focus();
+
+	GridPosition GetGridPosition(int x, int y) {
+		const int pointX = x / (width / 4) + 1;
+		const int pointY = y / (height / 4) + 1;
+
+		GridPosition pos;
+
+		/*
+			---------------
+			|1,1| 2,1 |3,1|
+			|---|-----|---|
+			|1,2| 2,2 |3,2|
+			|---|-----|---|
+			|1,3| 2,3 |3,3|
+			---------------
+		*/
+
+		if (!!(pointX & 0b100))
+			pos.x = PositionPart.Third;
+		else if (!!(pointX & 0b10))
+			pos.x = PositionPart.Second;
+		else if (!!(pointX & 0b1))
+			pos.x = PositionPart.First;
+
+		if (!!(pointY & 0b100))
+			pos.y = PositionPart.Third;
+		else if (!!(pointY & 0b10))
+			pos.y = PositionPart.Second;
+		else if (!!(pointY & 0b1))
+			pos.y = PositionPart.First;
+
+		return pos;
+	}
 
 	@property ref.Screen Screen() {
 		return screen;
