4bcc512a87b2f4ece37aca729930e51d93e181d8
Dylan Dunn <32021277+dhd5076@users.noreply.github.com>
2017-10-23 08:19:34 -0400

    Changed BG image (#11)
    * Changed BG image and host
    
    * Changed BG image to 1920x1080 res

--- source/app.d
+++ source/app.d
@@ -47,7 +47,7 @@ int main(string[] args) {
 		if (Xephyr)
 			kill(Xephyr);
 
-	spawnProcess(["feh", "--bg-scale", "http://wild.tk/DWinBG.png"]);
+	spawnProcess(["feh", "--bg-scale", "https://wallpaperscraft.com/image/black_light_dark_figures_73356_1920x1080.jpg"]);
 
 	auto dwin = new DWin(display);
 	scope (exit)
